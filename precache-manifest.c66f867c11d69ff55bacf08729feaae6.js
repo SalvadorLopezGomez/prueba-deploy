db95d469afb386b4db62416f640246a7c7e50a58
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-01 09:23:21 +0000

    Deploy SalvadorLG/prueba-deploy to github.com/SalvadorLG/prueba-deploy.git:gh-pages

---
+++ precache-manifest.c66f867c11d69ff55bacf08729feaae6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90d2888eb8a2b36c418e",
+    "url": "/prueba-deploy/css/app.b130a053.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/prueba-deploy/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a73de91cc9293cb39185d3ef6f779ebc",
+    "url": "/prueba-deploy/index.html"
+  },
+  {
+    "revision": "e9e7d8667ef6762d9171",
+    "url": "/prueba-deploy/js/about.030d6059.js"
+  },
+  {
+    "revision": "90d2888eb8a2b36c418e",
+    "url": "/prueba-deploy/js/app.681303fd.js"
+  },
+  {
+    "revision": "87799f30b1e551a41c75",
+    "url": "/prueba-deploy/js/chunk-vendors.9e69d06a.js"
+  },
+  {
+    "revision": "8671d47f8d95e84cb1d661276f91dc20",
+    "url": "/prueba-deploy/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/prueba-deploy/robots.txt"
+  }
+]);
